39a1c0147f12c307acea811838c5ed99b9946a55
izzahaemo <izzah.aemo@gmail.com>
2020-09-03 16:43:35 +0700

    edit tulisan timeline

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -53,7 +53,7 @@
     </div>
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Data Timeline</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Timeline REOR</h6>
         </div>
         <div class="card-body">
             <div class="timeline">
