251fbcf5a7e6e6ac9373892571526f1dbb3daf90
izzahaemo <izzah.aemo@gmail.com>
2020-09-03 17:24:20 +0700

    yo

--- application/views/admin/account.php
+++ application/views/admin/account.php
@@ -21,7 +21,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Akun</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Akun.</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $all['total']['total'] ?></div>
                         </div>
                         <div class="col-auto">
